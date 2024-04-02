b3a4530b37304d40b469c3ad0b9e648279c4faf8
Hernan Ferreras <hernanferreras@icloud.com>
2024-04-02 10:46:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ def read_int(prompt, min, max):
     # Escribe tu código aquí.
     #
     
-    try:
-        a = int(input(prompt))
-        assert (a >= min and a <= max)
-        return a
+    while prompt:
+        try:
+            a = int(input(prompt))
+            assert (a >= min and a <= max)
+            return a
         
-    except AssertionError:
-        print("Error: el valor no está dentro del rango permitido ("+str(min)+".."+str(max)+")")
+        except AssertionError:
+            print("Error: el valor no está dentro del rango permitido ("+str(min)+".."+str(max)+")")
     
-    except ValueError:
-        print("Error: entrada incorrecta")
+        except ValueError:
+            print("Error: entrada incorrecta")
         
-    read_int(prompt, min, max) 
-    
 v = read_int("Ingresa un numero entre -10 a 10: ", -10, 10)
 
 print("El número es:", v)
